3dfebc9dd34402c8d99c2b64680377c92bf43599
Jacek <jmonka@edu.cdv.pl>
2024-12-02 18:52:23 +0100

    Configure Vite Dev local environment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+const port = 5173;
+const origin = `${process.env.DDEV_PRIMARY_URL}:${port}`;
 
 export default defineConfig({
     plugins: [
@@ -17,4 +19,10 @@ export default defineConfig({
             },
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: port,
+        strictPort: true,
+        origin: origin
+    },
 });
